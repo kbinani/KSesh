d3f386045026e11fc400ea54c2f451a675913b5d
kbinani <kbinani.bt@gmail.com>
2024-07-20 18:37:40 +0900

    Add placeholder text

--- src/component/app.tsx
+++ src/component/app.tsx
@@ -1,18 +1,22 @@
 import * as React from "react";
-import { ChangeEvent, FC, useState } from "react";
+import { ChangeEvent, FC, useEffect, useState } from "react";
 import { ContentComponent } from "./content";
 import { Content } from "../content";
 
 export const App: FC = ({}) => {
   const [content, setContent] = useState<Content>(new Content(""));
+  const [changed, setChanged] = useState(false);
   const onChange = (ev: ChangeEvent<HTMLTextAreaElement>) => {
     const text = ev.target.value;
-    if (!text) {
-      return;
-    }
     const content = new Content(text);
     setContent(content);
+    setChanged(true);
   };
+  const placeholder = "Y3 Y1A";
+  useEffect(() => {
+    const text = new Content(placeholder);
+    setContent(text);
+  }, []);
   return (
     <div style={{ display: "flex", width: "100%", height: "100%" }}>
       <textarea
@@ -24,13 +28,19 @@ export const App: FC = ({}) => {
           outline: "none",
           border: "none",
         }}
+        autoFocus={true}
         spellCheck={false}
-        defaultValue={"Y3 A1"}
+        placeholder={changed ? undefined : placeholder}
         onChange={onChange}
       />
       <div
         className="hieroglyphs"
-        style={{ width: "50%", height: "100%", fontSize: "48px" }}
+        style={{
+          width: "50%",
+          height: "100%",
+          fontSize: "48px",
+          opacity: changed ? 1 : 0.5,
+        }}
       >
         <ContentComponent content={content} />
       </div>
