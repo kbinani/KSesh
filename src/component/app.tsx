78a203ce0ab70a1bc0a89bce698b4ce5b27934e6
kbinani <kbinani.bt@gmail.com>
2024-07-20 23:22:18 +0900

    Typing suggestion

--- src/component/app.tsx
+++ src/component/app.tsx
@@ -1,5 +1,12 @@
 import * as React from "react";
-import { ChangeEvent, FC, useEffect, useRef, useState } from "react";
+import {
+  ChangeEvent,
+  FC,
+  SyntheticEvent,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { ContentComponent } from "./content";
 import { Content } from "../content";
 
@@ -9,6 +16,7 @@ export const App: FC = ({}) => {
   const [content, setContent] = useState<Content>(new Content(""));
   const [changed, setChanged] = useState(false);
   const [activeSignListTab, setActiveSignListTab] = useState("typing");
+  const [typing, setTyping] = useState("A1");
   const textarea = useRef<HTMLTextAreaElement>(null);
   const onChange = (ev: ChangeEvent<HTMLTextAreaElement>) => {
     const text = ev.target.value;
@@ -16,23 +24,77 @@ export const App: FC = ({}) => {
     setContent(content);
     setChanged(true);
   };
+  const onSelect = (ev: SyntheticEvent<HTMLTextAreaElement>) => {
+    if (!textarea.current) {
+      return;
+    }
+    const { value, selectionStart, selectionEnd } = textarea.current;
+    const s = value.substring(0, selectionStart);
+    let found = -1;
+    for (let i = s.length - 1; i >= 0; i--) {
+      const c = s[i];
+      if (!/[0-9]/.test(c)) {
+        found = i;
+        break;
+      }
+    }
+    const search =
+      s.substring(found) + value.substring(selectionStart, selectionEnd);
+    if (/^[A-Z]/.test(search)) {
+      setTyping(search);
+    } else {
+      setTyping("");
+    }
+  };
   const onClickSign = (id: string, sign: string) => {
     if (!textarea.current) {
       return;
     }
-    let insert = id;
     const { value, selectionStart, selectionEnd } = textarea.current;
-    if (value.substring(selectionEnd, selectionEnd + 1) !== " ") {
-      insert += " ";
+    if (
+      typing.length > 0 &&
+      value.substring(
+        selectionStart - typing.length,
+        selectionStart + typing.length,
+      ) === typing
+    ) {
+      let insert = id;
+      if (
+        value.substring(
+          selectionStart + typing.length,
+          selectionStart + typing.length + 1,
+        ) !== " "
+      ) {
+        insert += " ";
+      }
+      const next =
+        value.substring(0, selectionStart - typing.length) +
+        insert +
+        value.substring(selectionStart + typing.length);
+      textarea.current.value = next;
+      const location = selectionStart - typing.length + insert.length;
+      textarea.current.setSelectionRange(location, location);
+      const content = new Content(next);
+      setContent(content);
+    } else {
+      let insert = id;
+      if (value.substring(selectionEnd, selectionEnd + 1) !== " ") {
+        insert += " ";
+      }
+      const next =
+        value.substring(0, selectionStart) +
+        insert +
+        value.substring(selectionEnd);
+      textarea.current.setRangeText(
+        insert,
+        selectionStart,
+        selectionEnd,
+        "end",
+      );
+      const content = new Content(next);
+      setContent(content);
     }
-    const next =
-      value.substring(0, selectionStart) +
-      insert +
-      value.substring(selectionEnd);
-    textarea.current.setRangeText(insert, selectionStart, selectionEnd, "end");
     textarea.current.focus();
-    const content = new Content(next);
-    setContent(content);
     setChanged(true);
   };
   useEffect(() => {
@@ -78,6 +140,7 @@ export const App: FC = ({}) => {
               spellCheck={false}
               placeholder={changed ? undefined : placeholder}
               onChange={onChange}
+              onSelect={onSelect}
             />
           </div>
         </div>
@@ -128,11 +191,19 @@ export const App: FC = ({}) => {
         >
           {Content.signs
             .filter(([s, _]) => {
-              const found = /[0-9]/.exec(s);
-              if (!found) {
-                return false;
+              if (activeSignListTab === "typing") {
+                if (typing === "") {
+                  return false;
+                } else {
+                  return s.startsWith(typing);
+                }
+              } else {
+                const found = /[0-9]/.exec(s);
+                if (!found) {
+                  return false;
+                }
+                return s.substring(0, found.index) === activeSignListTab;
               }
-              return s.substring(0, found.index) === activeSignListTab;
             })
             .map(([id, sign], index) => (
               <div
