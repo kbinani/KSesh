1c55ae3fe051e80637f72ab1aa49037b7dfbf58c
kbinani <kbinani.bt@gmail.com>
2024-07-20 21:48:23 +0900

    Add sign list tab

--- src/component/app.tsx
+++ src/component/app.tsx
@@ -28,9 +28,9 @@ export const App: FC = ({}) => {
     >
       <div
         style={{
-          height: "22px",
-          lineHeight: "22px",
-          fontSize: "16px",
+          height: "26px",
+          lineHeight: "26px",
+          fontSize: "20px",
           paddingLeft: "4px",
           backgroundColor: "#444",
           color: "#fff",
@@ -38,23 +38,25 @@ export const App: FC = ({}) => {
       >
         WSesh
       </div>
-      <div style={{ display: "flex", height: "100%" }}>
-        <div style={{ width: "50%", padding: 4 }}>
-          <textarea
-            style={{
-              width: "100%",
-              height: "100%",
-              fontSize: "20px",
-              resize: "none",
-              outline: "none",
-              border: "none",
-              padding: 0,
-            }}
-            autoFocus={true}
-            spellCheck={false}
-            placeholder={changed ? undefined : placeholder}
-            onChange={onChange}
-          />
+      <div style={{ display: "flex", height: "50%" }}>
+        <div style={{ display: "flex", width: "50%" }}>
+          <div style={{ padding: "4px" }}>
+            <textarea
+              style={{
+                width: "100%",
+                height: "100%",
+                fontSize: "20px",
+                resize: "none",
+                outline: "none",
+                border: "none",
+                padding: 0,
+              }}
+              autoFocus={true}
+              spellCheck={false}
+              placeholder={changed ? undefined : placeholder}
+              onChange={onChange}
+            />
+          </div>
         </div>
         <div
           style={{
@@ -64,10 +66,31 @@ export const App: FC = ({}) => {
             backgroundColor: "#ccc",
           }}
         >
-          <div style={{ padding: 4 }}>
+          <div style={{ padding: "8px" }}>
             <ContentComponent content={content} fontSize={48} />
           </div>
         </div>
+      </div>
+      <div style={{ display: "flex", flexDirection: "column", height: "50%" }}>
+        <div
+          className="signListTab"
+          style={{
+            display: "flex",
+            height: "26px",
+            lineHeight: "26px",
+            paddingLeft: "4px",
+            backgroundColor: "#444",
+            overflowY: "scroll",
+          }}
+        >
+          <div className="signListTabButton">typing</div>
+          {Content.categories.map((cat, index) => (
+            <div className="signListTabButton" key={index}>
+              {cat}
+            </div>
+          ))}
+        </div>
+        <div>content</div>
       </div>
     </div>
   );
