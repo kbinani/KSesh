e0d6ab4daf6e837c2bb0c5f6f399c97d6dce9b1a
kbinani <kbinani.bt@gmail.com>
2024-07-21 23:02:12 +0900

    Add some customizations for harfbuzz.ts

--- src/harfbuzz.ts
+++ src/harfbuzz.ts
@@ -10,36 +10,70 @@ class HarfBuzzExports {
   readonly utf8Encoder: TextEncoder;
 
   //exported HarfBuzz methods
-  readonly malloc: (length: number) => Pointer
-  readonly free: (ptr: Pointer) => void
-  readonly free_ptr: () => Pointer
-  readonly hb_blob_create: (data: Pointer, length: number, memoryMode: number, useData: Pointer, destroyFunction: Pointer) => Pointer
-  readonly hb_blob_destroy: (ptr: Pointer) => void
-  readonly hb_face_create: (blobPtr: Pointer, index: number) => Pointer
-  readonly hb_face_get_upem: (facePtr: Pointer) => number
-  readonly hb_face_destroy: (ptr: Pointer) => void
-  readonly hb_font_create: (facePtr: Pointer) => Pointer
-  readonly hb_font_set_scale: (fontPtr: Pointer, xScale: number, yScale: number) => void
-  readonly hb_font_destroy: (ptr: Pointer) => void
-  readonly hb_face_collect_unicodes: (facePtr: Pointer, setPtr: Pointer) => void
-  readonly hb_set_create: () => Pointer
-  readonly hb_set_destroy: (setPtr: Pointer) => void
-  readonly hb_set_get_population: (setPtr: Pointer) => number
+  readonly malloc: (length: number) => Pointer;
+  readonly free: (ptr: Pointer) => void;
+  readonly free_ptr: () => Pointer;
+  readonly hb_blob_create: (
+    data: Pointer,
+    length: number,
+    memoryMode: number,
+    useData: Pointer,
+    destroyFunction: Pointer,
+  ) => Pointer;
+  readonly hb_blob_destroy: (ptr: Pointer) => void;
+  readonly hb_face_create: (blobPtr: Pointer, index: number) => Pointer;
+  readonly hb_face_get_upem: (facePtr: Pointer) => number;
+  readonly hb_face_destroy: (ptr: Pointer) => void;
+  readonly hb_font_create: (facePtr: Pointer) => Pointer;
+  readonly hb_font_set_scale: (
+    fontPtr: Pointer,
+    xScale: number,
+    yScale: number,
+  ) => void;
+  readonly hb_font_destroy: (ptr: Pointer) => void;
+  readonly hb_face_collect_unicodes: (
+    facePtr: Pointer,
+    setPtr: Pointer,
+  ) => void;
+  readonly hb_set_create: () => Pointer;
+  readonly hb_set_destroy: (setPtr: Pointer) => void;
+  readonly hb_set_get_population: (setPtr: Pointer) => number;
   readonly hb_set_next_many: (
     setPtr: Pointer,
     greaterThanUnicodePtr: Pointer,
     outputU32ArrayPtr: Pointer,
     size: number,
-  ) => number
-  readonly hb_buffer_create: () => Pointer
-  readonly hb_buffer_add_utf8: (bufferPtr: Pointer, stringPtr: Pointer, stringLength: number, itemOffset: number, itemLength: number) => void
-  readonly hb_buffer_guess_segment_properties: (bufferPtr: Pointer) => void
-  readonly hb_buffer_set_direction: (bufferPtr: Pointer, direction: number) => void
-  readonly hb_shape: (fontPtr: Pointer, bufferPtr: Pointer, features: any, numFeatures: number) => void
-  readonly hb_buffer_get_length: (bufferPtr: Pointer) => number
-  readonly hb_buffer_get_glyph_infos: (bufferPtr: Pointer, length: number) => any
-  readonly hb_buffer_get_glyph_positions: (bufferPtr: Pointer, length: number) => any
-  readonly hb_buffer_destroy: (bufferPtr: Pointer) => void
+  ) => number;
+  readonly hb_buffer_create: () => Pointer;
+  readonly hb_buffer_add_utf8: (
+    bufferPtr: Pointer,
+    stringPtr: Pointer,
+    stringLength: number,
+    itemOffset: number,
+    itemLength: number,
+  ) => void;
+  readonly hb_buffer_guess_segment_properties: (bufferPtr: Pointer) => void;
+  readonly hb_buffer_set_direction: (
+    bufferPtr: Pointer,
+    direction: number,
+  ) => void;
+  readonly hb_buffer_set_script: (bufferPtr: Pointer, script: number) => void;
+  readonly hb_shape: (
+    fontPtr: Pointer,
+    bufferPtr: Pointer,
+    features: any,
+    numFeatures: number,
+  ) => void;
+  readonly hb_buffer_get_length: (bufferPtr: Pointer) => number;
+  readonly hb_buffer_get_glyph_infos: (
+    bufferPtr: Pointer,
+    length: number,
+  ) => any;
+  readonly hb_buffer_get_glyph_positions: (
+    bufferPtr: Pointer,
+    length: number,
+  ) => any;
+  readonly hb_buffer_destroy: (bufferPtr: Pointer) => void;
 
   constructor(exports: any) {
     this.heapu8 = new Uint8Array(exports.memory.buffer);
@@ -65,18 +99,19 @@ class HarfBuzzExports {
     this.hb_font_destroy = exports.hb_font_destroy;
     this.hb_buffer_create = exports.hb_buffer_create;
     this.hb_buffer_add_utf8 = exports.hb_buffer_add_utf8;
-    this.hb_buffer_guess_segment_properties = exports.hb_buffer_guess_segment_properties;
+    this.hb_buffer_guess_segment_properties =
+      exports.hb_buffer_guess_segment_properties;
     this.hb_buffer_set_direction = exports.hb_buffer_set_direction;
+    this.hb_buffer_set_script = exports.hb_buffer_set_script;
     this.hb_shape = exports.hb_shape;
     this.hb_buffer_get_length = exports.hb_buffer_get_length;
     this.hb_buffer_get_glyph_infos = exports.hb_buffer_get_glyph_infos;
     this.hb_buffer_get_glyph_positions = exports.hb_buffer_get_glyph_positions;
     this.hb_buffer_destroy = exports.hb_buffer_destroy;
   }
-
 }
 
-let hb: HarfBuzzExports;
+export let hb: HarfBuzzExports;
 
 class CString {
   readonly ptr: Pointer;
@@ -100,7 +135,13 @@ export class HarfBuzzBlob {
   constructor(data: Uint8Array) {
     let blobPtr = hb.malloc(data.length);
     hb.heapu8.set(data, blobPtr);
-    this.ptr = hb.hb_blob_create(blobPtr, data.byteLength, HB_MEMORY_MODE_WRITABLE, blobPtr, hb.free_ptr());
+    this.ptr = hb.hb_blob_create(
+      blobPtr,
+      data.byteLength,
+      HB_MEMORY_MODE_WRITABLE,
+      blobPtr,
+      hb.free_ptr(),
+    );
   }
 
   destroy() {
@@ -108,25 +149,21 @@ export class HarfBuzzBlob {
   }
 }
 
-function typedArrayFromSet<T extends 'u8' | 'u32' | 'i32'>(setPtr: Pointer, arrayType: T) {
+function typedArrayFromSet<T extends "u8" | "u32" | "i32">(
+  setPtr: Pointer,
+  arrayType: T,
+) {
   const heap = hb[`heap${arrayType}`];
   const bytesPerElment = heap.BYTES_PER_ELEMENT;
   const setCount = hb.hb_set_get_population(setPtr);
-  const arrayPtr = hb.malloc(
-    setCount * bytesPerElment,
-  );
+  const arrayPtr = hb.malloc(setCount * bytesPerElment);
   const arrayOffset = arrayPtr / bytesPerElment;
   const array = heap.subarray(
     arrayOffset,
     arrayOffset + setCount,
-  ) as typeof hb[`heap${T}`];
+  ) as (typeof hb)[`heap${T}`];
   heap.set(array, arrayOffset);
-  hb.hb_set_next_many(
-    setPtr,
-    HB_SET_VALUE_INVALID,
-    arrayPtr,
-    setCount,
-  );
+  hb.hb_set_next_many(setPtr, HB_SET_VALUE_INVALID, arrayPtr, setCount);
   return array;
 }
 
@@ -144,7 +181,7 @@ export class HarfBuzzFace {
   collectUnicodes() {
     const unicodeSetPtr = hb.hb_set_create();
     hb.hb_face_collect_unicodes(this.ptr, unicodeSetPtr);
-    const result = typedArrayFromSet(unicodeSetPtr, 'u32');
+    const result = typedArrayFromSet(unicodeSetPtr, "u32");
     hb.hb_set_destroy(unicodeSetPtr);
     return result;
   }
@@ -155,8 +192,8 @@ export class HarfBuzzFace {
 }
 
 export class HarfBuzzFont {
-  readonly ptr: Pointer
-  readonly unitsPerEM: number
+  readonly ptr: Pointer;
+  readonly unitsPerEM: number;
 
   constructor(face: HarfBuzzFace) {
     this.ptr = hb.hb_font_create(face.ptr);
@@ -172,17 +209,24 @@ export class HarfBuzzFont {
   }
 }
 
-export type HarfBuzzDirection = "ltr" | "rtl" | "ttb" | "btt"
+export type HarfBuzzDirection = "ltr" | "rtl" | "ttb" | "btt";
 
 class GlyphInformation {
-  readonly GlyphId: number
-  readonly Cluster: number
-  readonly XAdvance: number
-  readonly YAdvance: number
-  readonly XOffset: number
-  readonly YOffset: number
+  readonly GlyphId: number;
+  readonly Cluster: number;
+  readonly XAdvance: number;
+  readonly YAdvance: number;
+  readonly XOffset: number;
+  readonly YOffset: number;
 
-  constructor(glyphId: number, cluster: number, xAdvance: number, yAdvance: number, xOffset: number, yOffset: number) {
+  constructor(
+    glyphId: number,
+    cluster: number,
+    xAdvance: number,
+    yAdvance: number,
+    xOffset: number,
+    yOffset: number,
+  ) {
     this.GlyphId = glyphId;
     this.Cluster = cluster;
     this.XAdvance = xAdvance;
@@ -193,7 +237,7 @@ class GlyphInformation {
 }
 
 export class HarfBuzzBuffer {
-  readonly ptr: Pointer
+  readonly ptr: Pointer;
 
   constructor() {
     this.ptr = hb.hb_buffer_create();
@@ -210,7 +254,7 @@ export class HarfBuzzBuffer {
   }
 
   setDirection(direction: HarfBuzzDirection) {
-    let d = { "ltr": 4, "rtl": 5, "ttb": 6, "btt": 7 }[direction];
+    let d = { ltr: 4, rtl: 5, ttb: 6, btt: 7 }[direction];
     hb.hb_buffer_set_direction(this.ptr, d);
   }
 
@@ -220,60 +264,50 @@ export class HarfBuzzBuffer {
     var infosPtr32 = hb.hb_buffer_get_glyph_infos(this.ptr, 0) / 4;
     var positionsPtr32 = hb.hb_buffer_get_glyph_positions(this.ptr, 0) / 4;
     var infos = hb.heapu32.subarray(infosPtr32, infosPtr32 + 5 * length);
-    var positions = hb.heapi32.subarray(positionsPtr32, positionsPtr32 + 5 * length);
+    var positions = hb.heapi32.subarray(
+      positionsPtr32,
+      positionsPtr32 + 5 * length,
+    );
     for (var i = 0; i < length; ++i) {
-      result.push(new GlyphInformation(
-        infos[i * 5 + 0],
-        infos[i * 5 + 2],
-        positions[i * 5 + 0],
-        positions[i * 5 + 1],
-        positions[i * 5 + 2],
-        positions[i * 5 + 3]));
+      result.push(
+        new GlyphInformation(
+          infos[i * 5 + 0],
+          infos[i * 5 + 2],
+          positions[i * 5 + 0],
+          positions[i * 5 + 1],
+          positions[i * 5 + 2],
+          positions[i * 5 + 3],
+        ),
+      );
     }
     return result;
   }
 
   destroy() {
-    hb.hb_buffer_destroy(this.ptr)
+    hb.hb_buffer_destroy(this.ptr);
   }
-}
-
-export function shape(text: string, font: HarfBuzzFont, features: any): Array<GlyphInformation> {
-  let buffer = new HarfBuzzBuffer();
-  buffer.addText(text);
-  buffer.guessSegmentProperties();
-  buffer.shape(font, features);
-  let result = buffer.json();
-  buffer.destroy();
-  return result;
-}
-
-export function getWidth(text: string, font: HarfBuzzFont, fontSizeInPixel: number, features: any): number {
-  let scale = fontSizeInPixel / font.unitsPerEM;
-  let shapeResult = shape(text, font, features);
-  let totalWidth = shapeResult.map((glyphInformation) => {
-    return glyphInformation.XAdvance;
-  }).reduce((previous, current, i, arr) => {
-    return previous + current;
-  }, 0.0);
-
-  return totalWidth * scale;
 }
 
 export const harfbuzzFonts = new Map<string, HarfBuzzFont>();
 
 export function loadHarfbuzz(webAssemblyUrl: string): Promise<void> {
-  return fetch(webAssemblyUrl).then(response => {
-    return response.arrayBuffer();
-  }).then(wasm => {
-    return WebAssembly.instantiate(wasm);
-  }).then(result => {
-    //@ts-ignore
-    hb = new HarfBuzzExports(result.instance.exports);
-  });
+  return fetch(webAssemblyUrl)
+    .then((response) => {
+      return response.arrayBuffer();
+    })
+    .then((wasm) => {
+      return WebAssembly.instantiate(wasm);
+    })
+    .then((result) => {
+      //@ts-ignore
+      hb = new HarfBuzzExports(result.instance.exports);
+    });
 }
 
-export function loadAndCacheFont(fontName: string, fontUrl: string): Promise<void> {
+export function loadAndCacheFont(
+  fontName: string,
+  fontUrl: string,
+): Promise<void> {
   return fetch(fontUrl).then((response) => {
     return response.arrayBuffer().then((blob) => {
       let fontBlob = new Uint8Array(blob);
@@ -286,4 +320,13 @@ export function loadAndCacheFont(fontName: string, fontUrl: string): Promise<voi
       harfbuzzBlob.destroy();
     });
   });
+}
+
+export function HB_TAG(a: string, b: string, c: string, d: string): number {
+  return (
+    (a.charCodeAt(0) << 24) +
+    (b.charCodeAt(0) << 16) +
+    (c.charCodeAt(0) << 8) +
+    d.charCodeAt(0)
+  );
 }
