ae9bc21222f24ea46652f6cfc62378a532ad2140
kbinani <kbinani.bt@gmail.com>
2025-02-06 11:22:40 +0900

    Splitting the implementations into files

--- test/main.cpp
+++ test/main.cpp
@@ -6,6 +6,21 @@
 #include <optional>
 #include <variant>
 
+#include "otf/Type.hpp"
+#include "otf/io/InputStream.hpp"
+#include "otf/io/OutputStream.hpp"
+#include "otf/io/FileInputStream.hpp"
+#include "otf/io/FileOutputStream.hpp"
+#include "otf/io/ByteInputStream.hpp"
+#include "otf/io/ByteOutputStream.hpp"
+#include "otf/Tag.hpp"
+#include "otf/TableRecord.hpp"
+#include "otf/TableDirectory.hpp"
+#include "otf/table/Table.hpp"
+#include "otf/table/ReadonlyTable.hpp"
+#include "otf/table/FontHeaderTable.hpp"
+#include "otf/FontFile.hpp"
+
 #include "String.hpp"
 #include "Harfbuzz.hpp"
 #include "BinaryData.hpp"
