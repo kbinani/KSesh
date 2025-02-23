b44d9658ab99636160975e85bd22a4feeef0a11a
kbinani <kbinani.bt@gmail.com>
2025-02-24 08:14:50 +0900

    Add F2DOT14::{toFloat,FromFloat}

--- test/ContentTests.hpp
+++ test/ContentTests.hpp
@@ -1,5 +1,22 @@
 namespace ksesh::test {
 
+TEST_CASE("F2DOT14") {
+  using namespace std;
+  using namespace ksesh::otf;
+  vector<pair<int16_t, float>> data = {
+      {9583, 0.584899902f},
+      {-16383, -0.999938965f},
+  };
+  for (auto [i, f] : data) {
+    otf::F2DOT14 a;
+    a.data = i;
+    auto w = a.toFloat();
+    CHECK(a.toFloat() == f);
+    auto b = otf::F2DOT14::FromFloat(f);
+    CHECK(b.data == a.data);
+  }
+}
+
 TEST_CASE("research") {
   ksesh::otf::FileInputStream fis(juce::File::getCurrentWorkingDirectory().getChildFile("egyptiantext-COLR.ttf"));
   auto ff = ksesh::otf::FontFile::Read(fis);
