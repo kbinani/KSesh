a6153e346b857618366c329dfdbe28bb85c5b7e8
kbinani <kbinani.bt@gmail.com>
2025-07-07 00:12:35 +0900

    Remove ContentTests

--- test/main.cpp
+++ test/main.cpp
@@ -8,57 +8,15 @@
 
 #include "defer.hpp"
 
-#include "otf/Type.hpp"
-#include "otf/io/InputStream.hpp"
-#include "otf/io/OutputStream.hpp"
-#include "otf/io/FileInputStream.hpp"
-#include "otf/io/FileOutputStream.hpp"
-#include "otf/io/ByteInputStream.hpp"
-#include "otf/io/ByteOutputStream.hpp"
-#include "otf/Tag.hpp"
-#include "otf/TableRecord.hpp"
-#include "otf/TableDirectory.hpp"
-#include "otf/table/Table.hpp"
-#include "otf/table/ReadonlyTable.hpp"
-#include "otf/table/FontHeaderTable.hpp"
-#include "otf/table/MaximumProfileTable.hpp"
-#include "otf/table/IndexToLocationTable.hpp"
-#include "otf/table/GlyphDataTable.hpp"
-#include "otf/table/PostScriptTable.hpp"
-#include "otf/table/OS2AndWindowsMetricsTable.hpp"
-#include "otf/table/HorizontalHeaderTable.hpp"
-#include "otf/table/HorizontalMetricsTable.hpp"
-#include "otf/FontFile.hpp"
-
 #include "String.hpp"
 #include "Harfbuzz.hpp"
 #include "BinaryData.hpp"
 #include "Insertions.hpp"
 #include "SignList.hpp"
-//#include "PresentationSetting.hpp"
-//#include "Content.hpp"
-//#include "AppSetting.hpp"
-//#include "Clipboard.hpp"
-//#include "TextEditorComponent.hpp"
-//#include "HieroglyphComponent.hpp"
-//#include "Sign.hpp"
-//#include "SignListButtonContainer.hpp"
-//#include "BottomToolBar.hpp"
-//#include "SignListComponent.hpp"
-//#include "SplitterComponent.hpp"
-//#include "CommandID.hpp"
-//#include "MenuBarModel.hpp"
-//#include "AboutComponent.hpp"
-//#include "ExampleComponent.hpp"
-//#include "MainComponent.hpp"
-//#include "MainWindow.hpp"
-//#include "LookAndFeel.hpp"
 // clang-format on
 
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "doctest.h"
-
-#include "ContentTests.hpp"
 
 int main(int argc, char **argv) {
   doctest::Context context;
