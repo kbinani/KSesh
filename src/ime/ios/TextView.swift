e95c27a8bff465e1e721c7f674f27dd71bc3e4e4
kbinani <kbinani.bt@gmail.com>
2026-01-01 10:50:41 +0900

    Remove line feed character

--- src/ime/ios/TextView.swift
+++ src/ime/ios/TextView.swift
@@ -4,6 +4,20 @@ class TextView: UIView {
   struct Content: Equatable {
     let leading: String
     let trailing: String
+    
+    init(leading: String, trailing: String) {
+      if let last = leading.lastIndex(of: "\n") {
+        let from = leading.index(after: last)
+        self.leading = String(leading[from...])
+      } else {
+        self.leading = leading
+      }
+      if let first = trailing.firstIndex(of: "\n") {
+        self.trailing = String(trailing[..<first])
+      } else {
+        self.trailing = trailing
+      }
+    }
   }
 
   private enum Style {
